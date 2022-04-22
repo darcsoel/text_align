aa0c978572e0758b182400f029616f1c100085e4
Ross <rostikvunarchuk@gmail.com>
2022-04-23 00:08:53 +0300

    final solution

--- main.py
+++ main.py
@@ -1,16 +1,77 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.//
+import re
+from itertools import zip_longest
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f"Hi, {name}...")  # Press Ctrl+F8 to toggle the breakpoint.
+def convert_to_regular(text):
+    """Helper function"""
+
+    return re.sub(r"\s+", " ", text).strip()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == "__main__":
-    print_hi("PyCharm")
+def justify_line(line, width):
+    """
+    Justify line with
+    :param line:
+    :param width:
+    :return:
+    """
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    justified = ""
+    words = line.split(" ")
+    words_len = len(line.replace(" ", ""))
+    space_to_fill = width - words_len
+    spaces = [" "] * (len(words) - 1)
+    index = 0
+
+    while spaces:
+        if len("".join(spaces)) < space_to_fill:
+            spaces[index] = f"{spaces[index]} "
+            index += 1
+            if index == len(spaces):
+                index = 0
+        else:
+            break
+
+    for word, space in zip_longest(words, spaces):
+        if space is None:
+            space = ""
+
+        justified += f"{word}{space}"
+
+    return justified
+
+
+def justify(text: str, width: int):
+    """
+    Use spaces to fill in the gaps between words.
+    Each line should contain as many words as possible.
+    Use '\n' to separate lines.
+    Gap between words can't differ by more than one space.
+    Lines should end with a word not a space.
+    '\n' is not included in the length of a line.
+    Large gaps go first, then smaller ones ('Lorem--ipsum--dolor--sit-amet,' (2, 2, 2, 1 spaces)).
+    Last line should not be justified, use only one space between words.
+    Last line should not contain '\n'
+    Strings with one word do not need gaps ('somelongword\n').
+
+    :param text: input text
+    :param width: line length
+    :return: formatted text
+    """
+
+    words = convert_to_regular(text).split(" ")
+    sentences = [""]
+    index = 0
+
+    for word in words:
+        sentence = f"{sentences[index]} {word}".strip()
+
+        if len(sentence) > width:
+            sentences[index] = justify_line(sentences[index], width)
+            sentences.append(word)
+            index += 1
+        else:
+            sentences[index] = sentence
+
+    justified = "\n".join(sentences)
+    return justified
--- tests.py
+++ tests.py
@@ -1,1 +1,38 @@
-# import pytest
+from main import justify, convert_to_regular
+
+
+def test1():
+    assert justify("123 45 6", 7) == "123  45\n6"
+
+
+def test2():
+    expected = """Lorem  ipsum  dolor  sit amet,
+consectetur  adipiscing  elit.
+Vestibulum    sagittis   dolor
+mauris,  at  elementum  ligula
+tempor  eget.  In quis rhoncus
+nunc,  at  aliquet orci. Fusce
+at   dolor   sit   amet  felis
+suscipit   tristique.   Nam  a
+imperdiet   tellus.  Nulla  eu
+vestibulum    urna.    Vivamus
+tincidunt  suscipit  enim, nec
+ultrices   nisi  volutpat  ac.
+Maecenas   sit   amet  lacinia
+arcu,  non dictum justo. Donec
+sed  quam  vel  risus faucibus
+euismod.  Suspendisse  rhoncus
+rhoncus  felis  at  fermentum.
+Donec lorem magna, ultricies a
+nunc    sit    amet,   blandit
+fringilla  nunc. In vestibulum
+velit    ac    felis   rhoncus
+pellentesque. Mauris at tellus
+enim.  Aliquam eleifend tempus
+dapibus. Pellentesque commodo,
+nisi    sit   amet   hendrerit
+fringilla,   ante  odio  porta
+lacus,   ut   elementum  justo
+nulla et dolor."""
+
+    assert justify(convert_to_regular(expected), 30) == expected
